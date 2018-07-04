32888c687495bdb26c0f8fe5a1086e09c66f060b
sypachev_s_s <sypachev_s_s@mail.ru>
2018-07-04 17:52:05 +0500

    import 1.0.3 version

--- test/import.js
+++ test/import.js
@@ -9,3 +9,7 @@ console.log(mt.Image.GIF);
 let { Methods } = require('http-headers-js');
 
 console.log(Methods.POST);
+
+let { ContentEncodings } = require('http-headers-js');
+
+console.log(ContentEncodings.DEFLATE);
